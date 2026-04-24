acfce57447af960560630dece147c12cf29e5914
Abinashrout244 <routabinash3775@gmail.com>
2026-04-24 10:18:01 +0530

    learn:route Grouping

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="text-3xl font-semibold text-gray-300 flex h-screen justify-center items-center ">
+      This Your login Page
+    </div>
+  );
+};
+
+export default page;
