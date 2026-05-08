7a9b189557990a9ef7b5ff16627232b7ef4311ee
Abinashrout244 <routabinash3775@gmail.com>
2026-05-08 19:22:27 +0530

    active links

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -1,6 +1,27 @@
+"use client";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 export default function Header() {
+  const Links = [
+    {
+      name: "Home",
+      href: "/",
+    },
+    {
+      name: "Product",
+      href: "/products",
+    },
+    {
+      name: "Login",
+      href: "/login",
+    },
+    {
+      name: "Register",
+      href: "/register",
+    },
+  ];
+
+  const pathName = usePathname();
   return (
     <header className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
       {/* Logo */}
@@ -9,26 +30,20 @@ export default function Header() {
       {/* Navigation */}
       <nav>
         <ul className="flex gap-6">
-          <li>
-            <Link href="/" className="hover:text-gray-300">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/products" className="hover:text-gray-300">
-              Product
-            </Link>
-          </li>
-          <li>
-            <Link href="/login" className="hover:text-gray-300">
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link href="/register" className="hover:text-gray-300">
-              signup
-            </Link>
-          </li>
+          {Links.map((i, index) => {
+            const isActive =
+              pathName == i.href ||
+              (pathName.startsWith(i.href) && i.href != "/");
+            return (
+              <Link
+                href={i.href}
+                key={index}
+                className={`hover:text-gray-30 ${isActive ? "border-b-2 border-b-cyan-400 animate-bounce transition-all" : "border-none"}`}
+              >
+                <li>{i.name}</li>
+              </Link>
+            );
+          })}
         </ul>
       </nav>
     </header>
